25b6d7630a35d2876ea8e0b40d8417006e3ad3bb
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-02 21:45:25 -0230

    updated ArrayList methods

--- ArrayList/ArrayListExample.java
+++ ArrayList/ArrayListExample.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class ArrayListExample {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+
+        // Add elements to array list
         list.add(6);
         list.add(7);
         System.out.println(list);
@@ -26,6 +28,16 @@ public class ArrayListExample {
         java.util.Iterator<Integer> it = list.iterator();
         while (it.hasNext()) {
             System.out.println(it.next());
+        }
+
+        // Delete from array list
+        list.remove(2);
+
+        // Search in array list
+        if (list.contains(3)) {
+            System.out.println("Value 3 found in the list");
+        } else {
+            System.out.println("Value 3 not found in the list");
         }
     }
 }
