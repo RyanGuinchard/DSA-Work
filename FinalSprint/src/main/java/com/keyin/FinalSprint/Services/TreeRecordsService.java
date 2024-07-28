57e56a09d07bf36d37401ebdf562f7ad5c3a5b9c
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-07-28 12:15:51 -0230

    Completed Services to work on controllers

---
+++ FinalSprint/src/main/java/com/keyin/FinalSprint/Services/TreeRecordsService.java
@@ -0,0 +1,24 @@
+package com.keyin.FinalSprint.Services;
+
+import com.keyin.FinalSprint.Entities.TreeRecords;
+import com.keyin.FinalSprint.Repositories.TreeRecordsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TreeRecordsService {
+
+    @Autowired
+    private TreeRecordsRepository treeRecordsRepository;
+
+    public void saveTreeRecord(String inputNumbers, String treeStructure) {
+        TreeRecords treeRecords = new TreeRecords();
+        treeRecords.setInputNumbers(inputNumbers);
+        treeRecords.setTreeStructure(treeStructure);
+        treeRecordsRepository.save(treeRecords);
+    }
+
+    public <List> TreeRecords getAllTreeRecords() {
+        return (TreeRecords) treeRecordsRepository.findAll();
+    }
+}
